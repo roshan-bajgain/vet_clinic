63cb80a0182b764229bbd53c92dd0f37478898b7
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-03-23 18:22:11 +0545

    ADD foreign key referencing to species and owner

--- schema.sql
+++ schema.sql
@@ -24,3 +24,20 @@ CREATE TABLE species(
     name VARCHAR(100),
     PRIMARY KEY (species_id)
 );
+
+-- Remove column species
+ALTER TABLE animals DROP species;
+
+--Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals
+ADD species_id INT,
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species (id)
+
+--Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+ADD owner_id INT,
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners (id)
