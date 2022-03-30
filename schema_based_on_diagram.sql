1d37706fa7678c1c3749a6b7fb987752921a96f3
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-03-30 14:26:07 +0545

    Add table with other table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -27,3 +27,41 @@ CREATE TABLE invoices (
    REFERENCES medical_histories(id),
    PRIMARY KEY (id)
 );
+
+CREATE TABLE treatments (
+id INT GENERATED ALWAYS AS IDENTITY,
+type VARCHAR(255),
+name VARCHAR(255),
+PRIMARY KEY (id)
+); 
+
+CREATE TABLE invoice_items (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT,
+  treatment_id INT,
+  ADD CONSTRAINT fk_invoices
+  FOREIGN KEY (invoice_id)
+  REFERENCES invoices(id),
+  ADD CONSTRAINT fk_treatments
+  FOREIGN KEY (treatment_id)
+  REFERENCES treatments(id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE treatments_histories (
+id INT GENERATED ALWAYS AS IDENTITY,
+medical_history_id INT,
+treatment_id INT,
+ADD CONSTRAINT fk_medical_histories
+FOREIGN KEY (medical_history_id)
+REFERENCES medical_histories(id)
+ON DELETE RESTRICT ON UPDATE CASCADE,
+ADD CONSTRAINT fk_treatments
+FOREIGN KEY (treatment_id)
+REFERENCES treatments(id)
+ON DELETE RESTRICT ON UPDATE CASCADE,
+PRIMARY KEY (id)
+); 
