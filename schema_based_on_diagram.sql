98d5391ffe8e062fd00a900d9b600cc9aa367c14
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-03-30 14:05:30 +0545

    Create medical_history and add foreign key and primary_key

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,14 @@ CREATE TABLE patients (
     date_of_birth DATE,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at TIMESTAMP,
+    patient_id INT,
+    status VARCHAR(255),
+    ADD CONSTRAINT fk_patients,
+    FOREIGN_KEY (patient_id),
+    REFERENCES pateints(id),
+    PRIMARY KEY(id)   
+);
